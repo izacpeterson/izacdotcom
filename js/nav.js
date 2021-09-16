d535da42d0658979f6124d7a2a5108260501ee32
izacpeterson <izacpeterson@gmail.com>
2021-09-16 10:04:23 -0600

    worked on mobile styling

--- js/nav.js
+++ js/nav.js
@@ -15,13 +15,15 @@ let navOpen = false;
 $("#mobileHeader").on("click", () => {
   if (!navOpen) {
     $("#sideNav").css("left", "0");
-    $("#sideNav").css("max-width", "100vw");
-    $("#sideNav").css("width", "100vw");
+    $("#sideNav").css("max-width", "50vw");
+    $("#sideNav").css("width", "50vw");
+    $("#sideNav").css("box-shadow", "0 50px 50px #111");
     navOpen = true;
   } else if (navOpen) {
     $("#sideNav").css("left", "-60px");
     $("#sideNav").css("max-width", "60px");
     $("#sideNav").css("width", "60px");
+    $("#sideNav").css("box-shadow", "0 0 5px #111");
 
     navOpen = false;
   }
