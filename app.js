5507e12cc01bd071f80f27b7b630d7c67242597d
izacpeterson <izacpeterson@gmail.com>
2021-12-03 09:48:33 -0700

    added proj

--- app.js
+++ app.js
@@ -20,10 +20,8 @@ class Project {
 
 new Project("Solar System", "https://izacpeterson.github.io/solSystem/", "https://cdn-icons-png.flaticon.com/512/3533/3533265.png");
 new Project("ToDo App", "https://izacpeterson.github.io/todo/", "https://cdn-icons-png.flaticon.com/512/2666/2666505.png");
-new Project("DevFlix", "https://devflix.io/", "https://raw.githubusercontent.com/izacpeterson/mtech_movie/main/public/img/logo_512.png");
+new Project("DevFlix", "https://devflix.io/", "./img/logo_short_512.png");
 // new Project("WorldWide Holidays", "https://worldwide-holidays.netlify.app/", "https://image.flaticon.com/icons/png/512/814/814513.png");
 new Project("ISS Tracker", "https://izac-iss.netlify.app/", "https://image.flaticon.com/icons/png/512/1086/1086135.png");
+new Project("Prime Number Generator", "https://izacpeterson.github.io/primegen/", "https://cdn-icons-png.flaticon.com/512/752/752673.png");
 // new Project("Wind Chimes", "https://izac-windchimes.netlify.app/", "https://image.flaticon.com/icons/png/512/3946/3946256.png");
-
-// projectList.innerHTML += new Project("Prime Generator").render();
-// projectList.innerHTML += new Project("Golf ScoreCard").render();
