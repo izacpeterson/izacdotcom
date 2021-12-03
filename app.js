65dd448659a9915aa51aee48b654c2abd2ecf3ff
izacpeterson <izacpeterson@gmail.com>
2021-12-03 10:16:05 -0700

    added countdown app

--- app.js
+++ app.js
@@ -24,4 +24,5 @@ new Project("DevFlix", "https://devflix.io/", "./img/logo_short_512.png");
 // new Project("WorldWide Holidays", "https://worldwide-holidays.netlify.app/", "https://image.flaticon.com/icons/png/512/814/814513.png");
 new Project("ISS Tracker", "https://izac-iss.netlify.app/", "https://image.flaticon.com/icons/png/512/1086/1086135.png");
 new Project("Prime Number Generator", "https://izacpeterson.github.io/primegen/", "https://cdn-icons-png.flaticon.com/512/752/752673.png");
+new Project("Countdown", "https://izacpeterson.github.io/countdown/", "https://cdn-icons.flaticon.com/png/512/2408/premium/2408971.png?token=exp=1638551812~hmac=4b46ca504ca518177c5965cc5f00a3f0");
 // new Project("Wind Chimes", "https://izac-windchimes.netlify.app/", "https://image.flaticon.com/icons/png/512/3946/3946256.png");
