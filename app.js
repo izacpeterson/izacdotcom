07779d2e2440910d45fcd721fd8e711c56a57035
izacpeterson <izacpeterson@gmail.com>
2022-03-29 09:55:11 -0600

    major rework

--- app.js
+++ app.js
@@ -1,28 +1,34 @@
-let projectList = document.querySelector("#projectList");
-
+const projectList = [
+  { name: "Solar System", url: "url", desc: "A visual representation of our solar system", skills: ["HTML Canvas", "Javascript"] },
+  { name: "DevFlix", url: "url", desc: "An app to browse, save, and share your favorite movies", skills: ["Javascript", "HTML", "SASS", "Firebase"] },
+  { name: "Color Palettes", url: "url", desc: "Create and share color palettes", skills: ["ReactJS", "Semantic UI", "Firebase"] },
+];
 class Project {
-  constructor(name, url, img) {
+  constructor(name, url, desc, skills) {
     this.name = name;
     this.url = url;
-    this.img = img;
-    this.render();
+    this.desc = desc;
+    this.skills = skills;
   }
   render() {
-    projectList.innerHTML += `
-    <li class="project homeBtn">
-        <a href="${this.url}" target="_blank">
-            <h3>${this.name}</h3>
-            <!--<img src="${this.img}" />-->
-        </a>
-    </li>`;
+    document.querySelector("#projectList").innerHTML += `
+      <div class="project">
+        <h3>${this.name}</h3>
+        <span class='desc'>${this.desc}</span>
+        <h4>Skills used:</h4>
+        <ul>
+          ${this.skills
+            .map((skill) => {
+              return `<li>${skill}</li>`;
+            })
+            .join("")}
+        </ul>
+      </div>
+    
+    `;
   }
 }
 
-new Project("Solar System", "https://izacpeterson.github.io/solSystem/", "https://cdn-icons-png.flaticon.com/512/3533/3533265.png");
-new Project("ToDo App", "https://izacpeterson.github.io/todo/", "https://cdn-icons-png.flaticon.com/512/2666/2666505.png");
-new Project("DevFlix", "https://devflix.io/", "./img/logo_short_512.png");
-// new Project("WorldWide Holidays", "https://worldwide-holidays.netlify.app/", "https://image.flaticon.com/icons/png/512/814/814513.png");
-new Project("ISS Tracker", "https://izac-iss.netlify.app/", "https://image.flaticon.com/icons/png/512/1086/1086135.png");
-new Project("Prime Number Generator", "https://izacpeterson.github.io/primegen/", "https://cdn-icons-png.flaticon.com/512/752/752673.png");
-new Project("Countdown", "https://izacpeterson.github.io/countdown/", "https://cdn-icons.flaticon.com/png/512/2408/premium/2408971.png?token=exp=1638551812~hmac=4b46ca504ca518177c5965cc5f00a3f0");
-// new Project("Wind Chimes", "https://izac-windchimes.netlify.app/", "https://image.flaticon.com/icons/png/512/3946/3946256.png");
+projectList.forEach((project) => {
+  new Project(project.name, project.url, project.desc, project.skills).render();
+});
