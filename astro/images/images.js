8a02c9f24e8d4be9d3a499b4cc9aa9bad8b32041
izacpeterson <izacpeterson@gmail.com>
2022-07-18 00:31:30 -0600

    astro

---
+++ astro/images/images.js
@@ -0,0 +1,32 @@
+const images = [
+  {
+    src: "./images/VeilNeb.jpg",
+    text: "Veil Nebula",
+  },
+  {
+    src: "./images/RosetteNeb.jpg",
+    text: "Rosette Nebula",
+  },
+  {
+    src: "./images/HeartNeb.jpg",
+    text: "Heart Nebula",
+  },
+  {
+    src: "./images/CrescentNeb.jpg",
+    text: "Crescent Nebula",
+  },
+  {
+    src: "./images/AndromedaGal.jpg",
+    text: "Andromeda Galaxy",
+  },
+  {
+    src: "./images/HorseheadNeb.jpg",
+    text: "Horsehead Nebula",
+  },
+  {
+    src: "./images/OrionNeb.jpg",
+    text: "Orion Nebula",
+  },
+];
+
+export { images };
