3d7a96c9a8444f17c178ea1f37468c91a03b0e79
Izac Peterson <izacpeterson@gmail.com>
2023-05-08 18:29:57 -0600

    homepage ui

--- app/components/NavMenu.js
+++ app/components/NavMenu.js
@@ -5,18 +5,18 @@ export default function NavMenu() {
     <div className="flex flex-col items-center h-screen shadow-xl bg-zinc-800">
       {/* <Image src="logo.png" /> */}
       <img src="logo.png" className="w-24 p-4" />
-      <span className="text-center text-5xl text-green-500 font-bold p-4">Izac</span>
+      <span className="text-center text-5xl text-primary font-bold p-4">Izac</span>
       <nav className="flex flex-col h-full justify-center">
-        <Link href="/projects" className="p-4  hover:text-green-500 transition-all duration-200">
+        <Link href="/projects" className="p-4  hover:text-primary transition-all duration-200">
           Projects
         </Link>
-        <Link href="/projects" className="p-4  hover:text-green-500 transition-all duration-200">
+        <Link href="/projects" className="p-4  hover:text-primary transition-all duration-200">
           Skills
         </Link>
-        <Link href="/projects" className="p-4  hover:text-green-500 transition-all duration-200">
+        <Link href="/projects" className="p-4  hover:text-primary transition-all duration-200">
           About
         </Link>
-        <Link href="/projects" className="p-4  hover:text-green-500 transition-all duration-200">
+        <Link href="/projects" className="p-4  hover:text-primary transition-all duration-200">
           Contact
         </Link>
       </nav>
--- app/page.js
+++ app/page.js
@@ -7,11 +7,11 @@ export default function Home() {
       <div className="flex flex-col justify-center items-center w-full h-screen p-4">
         <span className="text-8xl">Hello, World!</span>
         <span className="text-8xl">
-          I'm <span className="text-green-500">Izac</span>
+          I'm <span className="text-primary">Izac</span>
         </span>
         <p className="text-xl">Web developer</p>
 
-        <button class="border border-green-500 text-green-500 p-4 m-4">See my work</button>
+        <button class="border border-primary text-primary p-4 m-4">See my work</button>
       </div>
     </main>
   );
