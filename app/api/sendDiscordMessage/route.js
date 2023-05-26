0abe8773046a0d05d15c73a085235d17c8afe475
izacpeterson <izacpeterson@gmail.com>
2023-05-26 14:09:28 -0600

    ip

--- app/api/sendDiscordMessage/route.js
+++ app/api/sendDiscordMessage/route.js
@@ -3,19 +3,12 @@ import { NextResponse } from "next/server";
 const url = "https://discordapp.com/api/webhooks/770724245214986261/qQTsNZsFXoiRopoc6EB3p0Ixt7427RkLd-BB_Cf4xKcWoOhFmvV1__2wJu590gkEAwmz";
 
 export async function GET(request) {
-  console.log(request.ip);
-  console.log(request.headers.get("x-forwarded-for").split(":")[3]);
   const { searchParams } = new URL(request.url);
   const message = searchParams.get("message");
   const date = new Date().toLocaleString();
-  let ip = request.headers.get("x-forwarded-for").split(":")[3];
-
-  let location = await fetch(`https://ipapi.co/${ip}/json/`);
-  location = await location.json();
-  console.log(location);
 
   const data = {
-    content: `**${date}**\n${message}\n${ip}\n${location}`,
+    content: `**${date}**\n${message}`,
   };
 
   const options = {
@@ -28,7 +21,8 @@ export async function GET(request) {
 
   fetch(url, options);
 
-  return NextResponse.json({ ip: request.ip });
-}
+  const clientIPs = [request.headers.get("cf-connecting-ip"), request.headers.get("x-forwarded-for"), request.headers.get("x-real-ip")];
+  console.log(clientIPs);
 
-function getIP(obj) {}
+  return NextResponse.json({ message: clientIPs });
+}
